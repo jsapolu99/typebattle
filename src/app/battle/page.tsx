b506fca2eaf49a9f3695a15d4752b5fa1807523d
jsapolu99 <109643051+jsapolu99@users.noreply.github.com>
2025-02-26 12:19:35 -0800

    Continued working on battle page mockup

--- src/app/battle/page.tsx
+++ src/app/battle/page.tsx
@@ -3,20 +3,29 @@ import Link from "next/link";
 
 export default function Battle() {
   return (
-    <body>
-    <Navbar/>
-    <main className={'flex items-start min-h-screen bg-[#a7c8da] p-3'}>
-      <div className={'content-center items-center'}>
-        <table className={'bg-white w-96 h-64 text-gray-900 '}>
-          <tbody className={'bg-gray-300'}>
-          <td>Filler text that the player will type in </td>
-          <td>
-            <input></input>
-          </td>
+    <main className={'bg-[#eeeeee] min-h-screen'}>
+      <Navbar/>
+      <div className={'flex justify-center'}>
+        <table className={'flex mx-0 my-16 bg-[#f6fbff] w-1/2 h-auto text-gray-900 justify-center rounded-lg p-0'}>
+          <tbody className={'p-0'}>
+          <tr className={'my-9 flex justify-center rounded'}>
+            <td className={''}>
+              Filler text that the user will type in
+            </td>
+          </tr>
+          <tr className={'my-9 flex justify-center min-w-full p-0 m-0'}>
+            <td className={'min-w-full'}>
+              <input
+                className={'bg-[#ffffff] min-w-full'}
+                type={'text'}
+                placeholder={'Type the above text here'}
+              />
+            </td>
+
+          </tr>
           </tbody>
         </table>
       </div>
     </main>
-    </body>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,16 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <body>
+    <main className={'min-h-screen bg-[#a7c8da]'}>
       <Navbar/>
-      <main className={'flex items-start min-h-screen bg-[#a7c8da] p-3'}>
-       <div className='w-auto h-auto rounded-lg bg-white shadow-lg py-4 px-4'>
-         <h1 className={'text-gray-900'}>TypeBatte - Battle your friends to assert dominance</h1>
-          <Link
-            href={'/battle'}
-            className='flex my-3 p-3 text-white md:w-40 bg-green-500 rounded-lg text-center hover:bg-green-400'
-          >
-            Enter a Typing Race</Link>
-       </div>
-      </main>
-    </body>
+      <div className='w-auto h-auto rounded-lg bg-white shadow-lg py-4 px-4'>
+        <h1 className={'text-gray-900'}>TypeBatte - Battle your friends to assert dominance</h1>
+        <Link
+          href={'/battle'}
+          className='flex my-3 p-3 text-white md:w-40 bg-green-500 rounded-lg text-center hover:bg-green-400'
+        >
+          Enter a Typing Race</Link>
+      </div>
+    </main>
   );
 }

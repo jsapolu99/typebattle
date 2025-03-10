86eb0abe6513b9d17e04c3991d927ebeb60ad129
jsapolu99 <109643051+jsapolu99@users.noreply.github.com>
2025-03-10 16:31:08 -0700

    Added Timer and win/loss conditions

--- src/app/battle/page.tsx
+++ src/app/battle/page.tsx
@@ -4,19 +4,13 @@ import BattleUI from "@/app/ui/battle/battle-ui";
 
 export default function Battle() {
 
-  const sampleText = 'Filler text for now! But what if it was a shit ton of text like this would it still possible look good on that format idk so that is what we are aiming to find out';
-
-
-  function roundOver (props) {
-    const showRoundLost = props.condition;
-
-    const showRoundComplete = props.condition;
-  }
+  const sampleText = 'Filler text for now';
+  const time = 10;
   return (
     <main className={'bg-[#eeeeee] min-h-screen'}>
       <Navbar/>
       <div className={'flex justify-center'}>
-        <BattleUI text={sampleText} />
+        <BattleUI text={sampleText} time={time} />
       </div>
     </main>
   );
--- src/app/ui/battle/battle-ui.tsx
+++ src/app/ui/battle/battle-ui.tsx
@@ -1,12 +1,21 @@
 'use client';
 
-import {RefObject, useRef, useState} from "react";
+import {RefObject, useRef, useState, useEffect} from "react";
 import clsx from "clsx";
 
-export default function BattleUI ({text}: {text: string}) {
+export default function BattleUI ({text, time}: {text: string, time: number}) {
   const health = useRef(3);
 
   const [userInput, setUserInput] = useState('');
+  const [seconds, setSeconds] = useState(time);
+  const [status, setStatus] = useState(false);
+
+  useEffect (() => {
+   const timer =
+     seconds > 0 && setInterval(() => setSeconds(seconds - 1), 1000);
+   return () => clearInterval(timer);
+  }, [seconds]);
+
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setUserInput(e.target.value);
   }
@@ -17,64 +26,81 @@ export default function BattleUI ({text}: {text: string}) {
 
   const currentIndex = userInput.length;
 
-  return (
-    <table className={'flex mx-0 my-16 bg-[#f6fbff] w-1/2 h-auto text-gray-900 justify-center rounded-lg'}>
-      <tbody className={'min-w-full p-3'}>
-      <tr className={'my-9 flex justify-center rounded'}>
-        <td>
-          <div className={'text-2xl tracking-widest'}>
-            {text.split("").map((char, index) => {
-              let className = "text-gray-400";
+  if (health.current > 0 && currentIndex < text.length && seconds > 0) {
+    return (
+      <table className={'flex mx-0 my-16 bg-[#f6fbff] w-1/2 h-auto tex-gray-900 justify-center rounded-lg'}>
+        <tbody className={'min-w-full p-3'}>
+        <tr>
+          <td className={'text-gray-900'}>
+            Time: {seconds}
+          </td>
+        </tr>
+        <tr className={'my-9 flex justify-center rounded'}>
+          <td>
+            <div className={'text-2xl tracking-widest'}>
+              {text.split("").map((char, index) => {
+                let className = "text-gray-400";
 
-              if (index < currentIndex) {
-                className = userInput[index] === char ? "text-green-400" : "bg-red-300";
-                if (className === 'bg-red-300') {
-                  updateHealth();
-                  console.log(health);
-                  if (health.current <= 0) {
-                    console.log('Game Over');
+                if (index < currentIndex) {
+                  className = userInput[index] === char ? "text-green-400" : "bg-red-300";
+                  if (className === 'bg-red-300') {
+                    updateHealth();
+                    console.log(health);
+                    if (health.current <= 0) {
+                      console.log('Game Over');
+                    }
                   }
                 }
-              }
-              if (index === currentIndex) {
-                className += " bg-blue-500 animate-pulse"; // Cursor effect
-              }
-              return (
-                <span key={index} className={className}>
+                if (index === currentIndex) {
+                  className += " bg-blue-500 animate-pulse"; // Cursor effect
+                }
+                return (
+                  <span key={index} className={className}>
               {char}
             </span>
-              );
-            })}
-          </div>
-        </td>
-      </tr>
-      <tr className={'my-9 flex justify-center min-w-full p-0'}>
-      <td className={'min-w-full'}>
-        <input
-          className={'bg-[#ffffff] min-w-full text-xl tracking-wide p-2'}
-          type={'text'}
-          value={userInput}
-          onChange={handleInputChange}
-          onPaste={(e) => e.preventDefault()}
-          onCopy={(e) => e.preventDefault()}
-          onCut={(e) => e.preventDefault()}
-          autoFocus={true}
-          placeholder={'Type the above text here when the battle begins!'}
-        />
-      </td>
-    </tr>
-    <tr className={'my-9 flex justify-center min-w-full p-0'}>
-      <td className={clsx('w-10 h-10 rounded-full mx-1',
-        {'bg-blue-500': health.current >= 3, 'bg-red-300': health.current < 3})}
-      />
-      <td className={clsx('w-10 h-10 rounded-full mx-1',
-        {'bg-blue-500': health.current >= 2, 'bg-red-300': health.current < 2})}
-      />
-      <td className={clsx('w-10 h-10 rounded-full mx-1',
-        {'bg-blue-500': health.current >= 1, 'bg-red-300': health.current < 1})}
-      />
-    </tr>
-      </tbody>
-    </table>
-  );
+                );
+              })}
+            </div>
+          </td>
+        </tr>
+        <tr className={'my-9 flex justify-center min-w-full p-0'}>
+          <td className={'min-w-full'}>
+            <input
+              className={'bg-[#ffffff] min-w-full text-xl tracking-wide p-2 text-gray-800'}
+              type={'text'}
+              value={userInput}
+              onChange={handleInputChange}
+              onPaste={(e) => e.preventDefault()}
+              onCopy={(e) => e.preventDefault()}
+              onCut={(e) => e.preventDefault()}
+              autoFocus={true}
+              placeholder={'Type the above text here when the battle begins!'}
+            />
+          </td>
+        </tr>
+        <tr className={'my-9 flex justify-center min-w-full p-0'}>
+          <td className={clsx('w-10 h-10 rounded-full mx-1',
+            {'bg-blue-500': health.current >= 3, 'bg-red-300': health.current < 3})}
+          />
+          <td className={clsx('w-10 h-10 rounded-full mx-1',
+            {'bg-blue-500': health.current >= 2, 'bg-red-300': health.current < 2})}
+          />
+          <td className={clsx('w-10 h-10 rounded-full mx-1',
+            {'bg-blue-500': health.current >= 1, 'bg-red-300': health.current < 1})}
+          />
+        </tr>
+        </tbody>
+      </table>
+    );
+  }
+  else if(currentIndex === text.length) {
+    return (
+      <p className={'text-black'}>You Win!</p>
+    );
+  }
+  else if (health.current <= 0 || seconds === 0) {
+    return (
+      <p className={'text-black'}>Round Over</p>
+    );
+  }
 }

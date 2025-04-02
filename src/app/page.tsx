f82e868826db706a4a1aa7b1885dd3729f0d62a2
jsapolu99 <109643051+jsapolu99@users.noreply.github.com>
2025-04-02 16:56:10 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,83 +2,68 @@
 import Navbar from "@/app/ui/navbar";
 import { v4 as uuidv4 } from 'uuid';
 import { useRouter } from 'next/navigation';
-import {FormEvent} from "react";
-import {Card, CardDescription, CardHeader, CardTitle, CardContent} from "@/components/ui/card";
+import { useState } from "react";
+import { Card, CardDescription, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import {toast} from "sonner";
-
+import { toast } from "sonner";
 
 export default function Home() {
+  const router = useRouter();
+  const [inviteCode, setInviteCode] = useState("");
 
-  const router = useRouter()
   function createGame() {
-    const inviteCode = uuidv4();
-    router.push(`/battle/${inviteCode}`);
+    const newInviteCode = uuidv4();
+    router.push(`/battle/${newInviteCode}`);
   }
 
-  function joinGame(e: FormEvent<HTMLFormEvent>) {
-    e.preventDefault()
-    const form = e.currentTarget;
-    const formData = new FormData(form);
-
-    const inviteCode = formData.get('inviteCode') as string;
-
+  function joinGame() {
     if (!inviteCode || inviteCode.length !== 36) {
-      toast('Please enter a valid invite code');
+      toast("Please enter a valid invite code");
       return;
-
     }
     router.push(`/battle/${inviteCode}`);
   }
 
   return (
-    <main className={'bg-[#eeeeee] min-h-screen'}>
-      <Navbar/>
-      <div className={'flex justify-center'}>
-        <Card className={'w-[350px] m-3'}>
+    <main className="bg-[#eeeeee] min-h-screen">
+      <Navbar />
+      <div className="flex justify-center">
+        <Card className="w-[350px] m-3">
           <CardHeader>
             <CardTitle>Welcome to Type Battle</CardTitle>
-            <CardDescription>Race against your friends and prove who’s the ultimate typing champion! Customize the game to match your style, challenge rivals in real-time, and climb the leaderboard. Only one can claim the top spot—will it be you?</CardDescription>
+            <CardDescription>
+              Race against your friends and prove who’s the ultimate typing champion! Customize the game to match your style, challenge rivals in real-time, and climb the leaderboard. Only one can claim the top spot—will it be you?
+            </CardDescription>
           </CardHeader>
         </Card>
       </div>
-      <div className={'flex justify-center'}>
-        <Card className={'w-[350px] m-3'}>
+      <div className="flex justify-center">
+        <Card className="w-[350px] m-3">
           <CardHeader>
             <CardTitle>Host Match</CardTitle>
-          <CardDescription>Host a new match</CardDescription>
+            <CardDescription>Host a new match</CardDescription>
           </CardHeader>
           <CardContent>
-            <Button
-
-              onClick={createGame}
-            >
-              Host Match
-            </Button>
+            <Button onClick={createGame}>Host Match</Button>
           </CardContent>
         </Card>
-        <Card className={'w-[350px] m-3'}>
+        <Card className="w-[350px] m-3">
           <CardHeader>
             <CardTitle>Join Match</CardTitle>
             <CardDescription>Enter the invite code to join a match</CardDescription>
           </CardHeader>
           <CardContent>
-            <form onSubmit={joinGame}>
-              <Input
-                className={'rounded my-3 font-bold p-3 text-gray-900 hover:bg-gray-100'}
-                type={'text'}
-                name={'inviteCode'}
-                placeholder={'Enter invite code'}
-              />
-              <Button
-                href={'/match-making/join'}
-              >
-                Join Match</Button>
-            </form>
+            <Input
+              className="rounded my-3 font-bold p-3 text-gray-900 hover:bg-gray-100"
+              type="text"
+              placeholder="Enter invite code"
+              value={inviteCode}
+              onChange={(e) => setInviteCode(e.target.value)}
+            />
+            <Button onClick={joinGame}>Join Match</Button>
           </CardContent>
         </Card>
-
       </div>
     </main>
   );

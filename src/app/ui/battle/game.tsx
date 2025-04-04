13b22224bb79762474a84a1573958f193d9b0ef0
jsapolu99 <109643051+jsapolu99@users.noreply.github.com>
2025-04-04 16:40:36 -0700

    Update game.tsx

--- src/app/ui/battle/game.tsx
+++ src/app/ui/battle/game.tsx
@@ -73,17 +73,6 @@ export default function Game({ gameId, name }: GameProps) {
   function setupListeners() {
     if(!ioInstance) return;
 
-    ioInstance.on("connect_error", (err) => {
-      // the reason of the error, for example "xhr poll error"
-      console.log(err.message);
-
-      // some additional description, for example the status code of the initial HTTP response
-      console.log(err.description);
-
-      // some additional context, for example the XMLHttpRequest object
-      console.log(err.context);
-    });
-
     ioInstance.on('connect', () => {
       console.log('Connected to server');
     })
